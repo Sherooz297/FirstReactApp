3d84d2b6e10c4d257763b98e489da33ff31413d3
Aimal Sherooz <aimalsherooz123@gmail.com>
2020-09-03 17:52:02 +0500

    add a img

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello from Aimal!</h1>
       <p>Im learning React.js</p>
+      <img src="https://www.google.com/search?q=img&rlz=1C1GCEJ_enPK893PK893&sxsrf=ALeKk02im4lO5xdPwn9BVj9GJFeF7ruJfA:1599137576735&tbm=isch&source=iu&ictx=1&fir=q7LnzSK5NqJZZM%252C7hrwuFj_6U-uEM%252C_&vet=1&usg=AI4_-kTJKTK0YvbWNb-IR0Cjp0gkfrlQjQ&sa=X&ved=2ahUKEwip1dTZg83rAhUYSRUIHRSqDRoQ9QF6BAgMEGQ&biw=1014&bih=569#imgrc=q7LnzSK5NqJZZM" alt="Girl in a jacket" width="500" height="600"></img>
 
     </div>
   );
